ed59884f9d91f6e1b2398c62dd8608d18b0336eb
Olga <63593441+OlGubkina@users.noreply.github.com>
2022-02-07 21:13:21 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+/**
+Используя Users.class из ДЗ 6(enums, занятие №7), создать ArrayList из 10 объектов Users, вывести на печать hashcode этих объектов
+        создать TreeMap объектов User из ранее созданного Arraylist и отсортировать их по priority и login в порядке возрастания.
+        (Вначале будут элементы с наименьшим priority, отсортированные по алфавиту)
+
+
+
+        UPD 01.02 Вместо TreeMap можно по желанию использовать TreeSet, такой вариант будет проще и понятнее
+ */
+ public class Main {
+    public static void main(String[] args) {
+
+	// write your code here
+    }
+}
